4ad5c331b8c8692cf72a945dcd89d14f2b47a8c7
rakibulhassan07 <rakibalhassan523@gmail.com>
2024-02-18 01:34:52 +0600

    added Full Game

--- aloha-clash.js
+++ aloha-clash.js
@@ -1,3 +1,50 @@
+
+
+
+
+function handleKeyboardPress(event){
+    
+    const playerPress=event.key;
+    if(playerPress==='Escape')
+    {
+        gameOver();
+    }
+    const randomAlphabet=document.getElementById('currentAlphabet')
+    const getAlphabet=randomAlphabet.innerText;
+    const getLowerAlphabet= getAlphabet.toLowerCase(); 
+    console.log(playerPress,getLowerAlphabet);
+
+    if(playerPress===getLowerAlphabet)
+    {   
+        const getScore=document.getElementById('currentScore');
+        const scoreText=getScore.innerText;
+        const score=parseInt(scoreText);
+        const increment=score+1;
+        getScore.innerText=increment;
+        
+      
+        
+        removeBackground(getLowerAlphabet);  
+        continueGame();
+    }
+    else {
+        const getLife=document.getElementById('currentLife');
+        const lifeText=getLife.innerText;
+        const life=parseInt(lifeText);
+        const discernment=life-1;
+        getLife.innerText=discernment;
+        if(discernment===0){
+            gameOver();
+          
+        }
+
+    }
+    
+
+    
+}
+document.addEventListener('keyup',handleKeyboardPress);
+
 function continueGame(){
     const alphabet=getRandomAlphabet();
     const currentAl=document.getElementById('currentAlphabet')
@@ -8,7 +55,24 @@ function continueGame(){
 function  play(){
 
     hideElementId('home');
+    hideElementId('scorePanel')
     showElementId('playground')
+    setGetLife('currentLife',5)
+    setGetLife('currentScore',0)
+
+    
     continueGame()
 }
- 
+   
+function gameOver(){
+    hideElementId('playground');
+    showElementId('scorePanel');
+    const lastScore=document.getElementById('currentScore')
+    const lastResult=lastScore.innerText
+    setGetLife('result',lastResult)
+    const lastText=getText('currentAlphabet')
+    removeBackground(lastText)
+     
+    
+    
+}
--- utility.js
+++ utility.js
@@ -22,3 +22,18 @@ function setBackground(elementId)
     const element =document.getElementById(elementId)
     element.classList.add('bg-orange-600')
 }
+
+function removeBackground(elementId)
+{
+    const element =document.getElementById(elementId)
+    element.classList.remove('bg-orange-600')
+}
+function setGetLife(element,value){
+    const getLife=document.getElementById(element);
+       getLife.innerText=value;
+}
+function getText(elementId){
+    const lastText=document.getElementById(elementId)
+    const text=lastText.innerText;
+    return text;
+}
